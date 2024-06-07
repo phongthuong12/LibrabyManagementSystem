4da0f51a32741acae3f086878484ace8232f77ad
phongthuong12 <90915750+phongthuong12@users.noreply.github.com>
2024-06-07 12:33:26 +0700

    update class Loan

--- LibraryManagement/src/model/Loan.java
+++ LibraryManagement/src/model/Loan.java
@@ -4,10 +4,71 @@
  */
 package model;
 
+import java.io.Serializable;
+import java.util.Date;
+
 /**
  *
  * @author Phong
  */
-public class Loan {
+public class Loan implements Serializable{
+    private String loadId;
+    private String bookId;
+    private String userId;
+    private Date borrowDate;
+    private Date returnDate;
+
+    public Loan(String loadId, String bookId, String userId, Date borrowDate, Date returnDate) {
+        this.loadId = loadId;
+        this.bookId = bookId;
+        this.userId = userId;
+        this.borrowDate = borrowDate;
+        this.returnDate = returnDate;
+    }
+
+    public String getLoadId() {
+        return loadId;
+    }
+
+    public void setLoadId(String loadId) {
+        this.loadId = loadId;
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Date getBorrowDate() {
+        return borrowDate;
+    }
+
+    public void setBorrowDate(Date borrowDate) {
+        this.borrowDate = borrowDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
     
+    @Override
+    public String toString() {
+        return "Loan [loadId=" + loadId + ", bookId=" + bookId + 
+               ", userId=" + userId + ", borrowDate=" + borrowDate + ", returnDate=" + returnDate + "]";
+    }
 }
