8758b9737117d5344c571749a80038b7fde43b53
phongthuong12 <90915750+phongthuong12@users.noreply.github.com>
2024-06-07 12:31:02 +0700

    update class User

--- LibraryManagement/src/model/User.java
+++ LibraryManagement/src/model/User.java
@@ -4,10 +4,81 @@
  */
 package model;
 
+import java.io.Serializable;
+import java.util.Date;
+
 /**
  *
  * @author Phong
  */
-public class User {
+public class User implements Serializable{
+    private String userId;
+    private String userName;
+    private Date dateOfBirth;
+    private String phoneNumber;
+    private String email;
+    private boolean activeUser;
+
+    public User(String userId, String userName, Date dateOfBirth, String phoneNumber, String email, boolean activeUser) {
+        this.userId = userId;
+        this.userName = userName;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.activeUser = activeUser;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNUmber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isActiveUser() {
+        return activeUser;
+    }
+
+    public void setActiveUser(boolean activeUser) {
+        this.activeUser = activeUser;
+    }
     
+    @Override
+    public String toString() {
+        return "User [userId=" + userId + ", name=" + userName + ", dateOfBirth=" + dateOfBirth + 
+               ", phoneNumber=" + phoneNumber + ", email=" + email + ", activeUser=" + activeUser + "]";
+    }
 }
